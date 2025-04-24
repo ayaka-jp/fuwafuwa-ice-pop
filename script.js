dc4bb7128b5685aa7c3ad97fe796685e1f58120b
Ayaka <abc.song003@gmail.com>
2025-04-24 16:21:05 +0900

    jsシンプル

--- script.js
+++ script.js
@@ -8,11 +8,3 @@ document.querySelectorAll('.ice-pop-container img').forEach(img => {
     }, 3000); // 3秒後に外す（animationの回数に合わせる）
   });
 });
-
-  // モバイル向けvh単位の修正
-  function setVH() {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  }
-  window.addEventListener('resize', setVH);
-  window.addEventListener('load', setVH);
